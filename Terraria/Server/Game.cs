03d128a6bec472add5e773ad1e0b4fbce8e8b1ed
The Steampunker <35268066+garmadaddy05@users.noreply.github.com>
2018-06-05 10:27:11 -0600

    Yes. I did it.

--- Terraria/Server/Game.cs
+++ Terraria/Server/Game.cs
@@ -1,8 +1,8 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: Terraria.Server.Game
-// Assembly: Terraria, Version=1.3.5.3, Culture=neutral, PublicKeyToken=null
-// MVID: 68659D26-2BE6-448F-8663-74FA559E6F08
-// Assembly location: H:\Steam\steamapps\common\Terraria\Terraria.exe
+﻿/** Decompiled with JetBrains decompiler
+ Type: Terraria.Server.Game
+ Assembly: Terraria, Version=1.3.5.3, Culture=neutral, PublicKeyToken=null
+ MVID: 68659D26-2BE6-448F-8663-74FA559E6F08
+**/ Assembly location: H:\Steam\steamapps\common\Terraria\Terraria.exe
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
