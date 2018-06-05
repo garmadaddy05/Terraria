22067d75668b8b57a35ae374229fedff0dfae185
The Steampunker <35268066+garmadaddy05@users.noreply.github.com>
2018-06-05 10:32:44 -0600

    Yes. I did it.

--- TerrariaServer/NATUPNPLib/IStaticPortMapping.cs
+++ TerrariaServer/NATUPNPLib/IStaticPortMapping.cs
@@ -1,8 +1,8 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: NATUPNPLib.IStaticPortMapping
-// Assembly: TerrariaServer, Version=1.3.5.3, Culture=neutral, PublicKeyToken=null
-// MVID: 8A63A7A2-328D-424C-BC9D-BF23F93646F7
-// Assembly location: H:\Steam\steamapps\common\Terraria\TerrariaServer.exe
+﻿/* Decompiled with JetBrains decompiler
+ Type: NATUPNPLib.IStaticPortMapping
+ Assembly: TerrariaServer, Version=1.3.5.3, Culture=neutral, PublicKeyToken=null
+ MVID: 8A63A7A2-328D-424C-BC9D-BF23F93646F7
+*/ Assembly location: H:\Steam\steamapps\common\Terraria\TerrariaServer.exe
 
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
